af0a3325d4b065e4cda8b3766d31ccca58d99fa6
joaopedro1422 <jpcros40414@gmail.com>
2025-05-22 21:01:47 -0300

    Adicionando webhooks2

--- src/app/qrcode/qrcode.component.ts
+++ src/app/qrcode/qrcode.component.ts
@@ -57,7 +57,7 @@ export class QrcodeComponent implements AfterViewInit{
       this.handleRouteParams(params);
         console.log('Elemento do canvas:', this.paginaId);
         this.qrCode.update({
-            data: `http://https://lovelink-frontenddeploy.vercel.app/${this.slug}/${this.paginaId}`
+            data: `https://lovelink-frontenddeploy.vercel.app/${this.slug}/${this.paginaId}`
         });
 
 
