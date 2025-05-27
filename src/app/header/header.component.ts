4a5d4e940afa206a1365bc1d997b60f5f849b065
joaopedro1422 <jpcros40414@gmail.com>
2025-05-26 21:44:44 -0300

    Ajustando responsividade Pagina inicial3

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-
+import { NgClass } from '@angular/common';
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgClass, NgIf],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+     menuAberto = false;
 
+      toggleMenu() {
+        this.menuAberto = !this.menuAberto;
+      }
 }
