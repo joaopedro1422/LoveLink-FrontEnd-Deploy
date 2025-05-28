c83d39626923f987a5a240368ad350f85a20c0fa
joaopedro1422 <jpcros40414@gmail.com>
2025-05-28 16:29:55 -0300

    Ajustando pagina de cadastro de paginas de parceiros10

--- src/app/youtube-player/youtube-player.component.ts
+++ src/app/youtube-player/youtube-player.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges , AfterViewInit} from '@angular/core';
 declare var YT: any;
 declare global {
   interface Window {
@@ -13,18 +13,18 @@ declare global {
   templateUrl: './youtube-player.component.html',
   styleUrl: './youtube-player.component.css'
 })
-export class YoutubePlayerComponent implements OnInit, OnChanges{
-    @Input() videoId!: string;
+export class YoutubePlayerComponent implements AfterViewInit, OnChanges{
+     @Input() videoId!: string;
   player: any;
   playerId = 'youtube-player-' + Math.floor(Math.random() * 100000);
 
-    ngOnInit() {
-      if (window.YT && window.YT.Player) {
-        this.initPlayer();
-      } else {
-        window.onYouTubeIframeAPIReady = () => this.initPlayer();
-      }
+  ngAfterViewInit() {
+    if (window.YT && window.YT.Player) {
+      this.initPlayer();
+    } else {
+      window.onYouTubeIframeAPIReady = () => this.initPlayer();
     }
+  }
 
   ngOnChanges(changes: SimpleChanges) {
     if (this.player && changes['videoId'] && !changes['videoId'].isFirstChange()) {
